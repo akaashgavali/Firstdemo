6cdb73e4e33b919ac4bd69856e37ef029e96e9d8
akaashgavali <akashgavali4546@gmail.com>
2022-09-30 14:40:17 +0530

    arithmatic and relational;

---
+++ Firstdemo/Arrithmatic.cs
@@ -0,0 +1,28 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Firstdemo
+//{
+//    internal class Arrithmatic
+//    {
+//        static void Main(string[] args)
+//      {
+//           Console.WriteLine("enter 2 numbers");
+//           int a = Convert.ToInt32(Console.ReadLine());
+//           int b = Convert.ToInt32(Console.ReadLine());
+//            int sum = a + b;
+//            Console.WriteLine(sum);
+//            int sub = a - b;
+//            Console.WriteLine(sub);
+//            int mul = a * b;
+//            Console.WriteLine(mul);
+
+
+//                }
+//            }
+        
+//    }
+
--- Firstdemo/Program.cs
+++ Firstdemo/Program.cs
@@ -1,31 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Firstdemo
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int year = 2022;
-            string month = "sept";
-            float percentage = 89.56f;
-            double a = 78.67;
-            //using + sign
-            Console.WriteLine("current yeat is " + year + "current month is " + month);
-            //using placeholder
-            Console.WriteLine("current year is {0} and current month is {1}", year, month);
-            //using interpolation
-            Console.WriteLine($"current year is {year}and month is {month}");
-            Console.WriteLine(year);
-            Console.WriteLine(month);
-            Console.WriteLine(percentage);
+//namespace Firstdemo
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int year = 2022;
+//            string month = "sept";
+//            float percentage = 89.56f;
+//            double a = 78.67;
+//            //using + sign
+//            Console.WriteLine("current yeat is " + year + "current month is " + month);
+//            //using placeholder
+//            Console.WriteLine("current year is {0} and current month is {1}", year, month);
+//            //using interpolation
+//            Console.WriteLine($"current year is {year}and month is {month}");
+//            Console.WriteLine(year);
+//            Console.WriteLine(month);
+//            Console.WriteLine(percentage);
             
             
-        }
-    }
-}
+//        }
+//    }
+//}
