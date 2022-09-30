b38cc2570c71daa498db29bb899d8e57311a86b6
akaashgavali <akashgavali4546@gmail.com>
2022-09-30 15:57:51 +0530

    commmit

--- Firstdemo/Relationalop1.cs
+++ Firstdemo/Relationalop1.cs
@@ -1,24 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Firstdemo
-{
-    internal class Relationalop1
-    {
-        static void Main(string[] args)
-        {
-            int a = 10, b = 10;
-            bool c = a == b;
-            Console.WriteLine(c);
-            bool d = a <= b;
-            Console.WriteLine(d);
-            bool e = a >= b;
-            Console.WriteLine(e);
-            bool f = a != b;
-            Console.WriteLine(f);
-        }
-    }
-}
+//namespace Firstdemo
+//{
+//    internal class Relationalop1
+//    {
+//        static void Main(string[] args)
+//        {
+//            int a = 10, b = 10;
+//            bool c = a == b;
+//            Console.WriteLine(c);
+//            bool d = a <= b;
+//            Console.WriteLine(d);
+//            bool e = a >= b;
+//            Console.WriteLine(e);
+//            bool f = a != b;
+//            Console.WriteLine(f);
+//        }
+//    }
+//}
