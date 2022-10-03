d8e932703540c3839f90df708cfcdb0787abb59b
akaashgavali <akashgavali4546@gmail.com>
2022-10-03 22:19:13 +0530

    commit

---
+++ Firstdemo/Switcgcaseassmnt3.cs
@@ -0,0 +1,50 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Firstdemo
+//{
+//    internal class Switcgcaseassmnt3
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("enter two numbers");
+//            int n1 = Convert.ToInt32(Console.ReadLine());
+
+//            int n2 = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("1. Addition");
+//            Console.WriteLine("2. substraction");
+//            Console.WriteLine("3. multiplication");
+//            Console.WriteLine("4. division");
+//            Console.WriteLine("5. mod");
+
+            
+//            int choice = Convert.ToInt32(Console.ReadLine());
+            
+
+//            switch (choice)
+//            {
+//                case 1:
+//                    Console.WriteLine(n1 + n2);
+//                    break;
+//                case 2:
+//                    Console.WriteLine(n1 - n2);
+//                    break;
+//                case 3:
+//                    Console.WriteLine(n1 * n2);
+//                    break;
+//                case 4:
+//                    Console.WriteLine(n1 / n2); 
+//                    break;
+//                case 5:
+//                    Console.WriteLine(n1 % n2);
+//                    break;
+//                default:
+//                    Console.WriteLine("wrong input");
+//                    break;
+//            }
+//        }
+//    }
+//}
---
+++ Firstdemo/Vowelconsanant.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firstdemo
+{
+    internal class Vowelconsanant
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter any alphabet");
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            switch(ch)
+            {
+                case 'a':
+                    Console.WriteLine("Alphabet is vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("Alphabet is vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("Alphabet is vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("Alphabet is vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("Alphabet is vowel");
+                    break;
+                case 'A':
+                    Console.WriteLine("Alphabet is vowel");
+                    break;
+                case 'E':
+                    Console.WriteLine("Alphabet is vowel");
+                    break;
+                case 'I':
+                    Console.WriteLine("Alphabet is vowel");
+                    break;
+                case 'O':
+                    Console.WriteLine("Alphabet is vowel");
+                    break;
+                case 'U':
+                    Console.WriteLine("Alphabet is vowel");
+                    break;
+                default:
+                    Console.WriteLine("Alphabet is consonant");
+                    break;
+            }
+
+        }
+    }
+}
