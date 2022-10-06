24d172afb1957ab9bac57d837d8a669360d4d6bc
akaashgavali <akashgavali4546@gmail.com>
2022-10-06 16:42:08 +0530

    commit

--- Firstdemo/Armstronghw3.cs
+++ Firstdemo/Armstronghw3.cs
@@ -1,39 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Firstdemo
-{
-    internal class Armstronghw3
-    {
-        static void Main(string[] args)
-        {
-            int num;
-            int result ;
-            int sum=0;
-            int rem;
-            Console.WriteLine("enter a number");
-             num = Convert.ToInt32(Console.ReadLine());
+//namespace Firstdemo
+//{
+//    internal class Armstronghw3
+//    {
+//        static void Main(string[] args)
+//        {
+//            int num;
+//            int result ;
+//            int sum=0;
+//            int rem;
+//            Console.WriteLine("enter a number");
+//             num = Convert.ToInt32(Console.ReadLine());
 
-            result = num;
+//            result = num;
 
-            while(num>0)
-            {
-                rem = num % 10;
-                sum = sum + rem * rem * rem;
-                num = num / 10;
-            }
-            if(result==sum)
-            {
-                Console.WriteLine("Armstrong number");
-            }
-            else
-            {
-                Console.WriteLine("Not Armstrong number");
-            }
-        }
+//            while(num>0)
+//            {
+//                rem = num % 10;
+//                sum = sum + rem * rem * rem;
+//                num = num / 10;
+//            }
+//            if(result==sum)
+//            {
+//                Console.WriteLine("Armstrong number");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Not Armstrong number");
+//            }
+//        }
 
-    }
-}
+//    }
+//}
---
+++ Firstdemo/Nestedloop2.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Firstdemo
+//{
+//    internal class Nestedloop2
+//    {
+//        static void Main(string[] args)
+//        {
+//            for (int i = 1; i <= 5; i++)
+
+//            {
+//                for (int j = 5; j >= i; j--)
+//                {
+//                    Console.Write(j);
+//                }
+//                Console.WriteLine();
+//            }
+//        }
+//    }
+//}
